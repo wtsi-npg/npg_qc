8808112973013cde67a00fcebd0cfc3d9db922e2
jmtcsngr <jmtc@sanger.ac.uk>
2015-04-09 11:38:07 +0100

    First try to expose runs through REST. Skeleton of first class using Catalyst::Controller::REST

---
+++ npg_qc_viewer/lib/npg_qc_viewer/Controller/Mqc_Run.pm
@@ -0,0 +1,70 @@
+package npg_qc_viewer::Controller::Mqc_Run;
+
+use Moose;
+use namespace::autoclean;
+use Readonly;
+
+BEGIN { extends 'Catalyst::Controller::REST' }
+
+with 'npg_qc_viewer::api::error';
+
+our $VERSION  = '0';
+
+sub mqc_run : Local : ActionClass('REST') { }
+
+1;
+
+=head1 NAME
+
+npg_qc_viewer::Controller::Mqc_Run
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Controller to expose runs through REST
+
+=head1 SUBROUTINES/METHODS 
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Readonly
+
+=item namespace::autoclean
+
+=item Moose
+
+=item Catalyst::Controller::REST
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2015 Genome Research Ltd.
+
+This file is part of NPG software.
+
+NPG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
