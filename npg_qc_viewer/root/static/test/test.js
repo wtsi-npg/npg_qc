245f11f51d6ef8b984ffc39f12c5130e0472f921
jmtcsngr <jmtc@sanger.ac.uk>
2015-05-15 19:19:51 +0100

    Fixing undefined comparisons

--- npg_qc_viewer/root/static/test/test.js
+++ npg_qc_viewer/root/static/test/test.js
@@ -12,15 +12,15 @@ var TestConfiguration = (function() {
 
 test('Object initialisation', function() {
   var obj = null;
-  ok(obj == undefined, "Variable is initially empty.");
+  ok(obj === undefined, "Variable is initially empty.");
   obj = new NPG.QC.LaneMQCControl();
   ok(obj !== undefined, "Variable is now an instance.");
-  ok(obj.index == undefined, "Object has no initial index.");
+  ok(obj.index === undefined, "Object has no initial index.");
   obj = new NPG.QC.LaneMQCControl(0, new TestConfiguration());
   ok(obj !== undefined, "Variable is now a new instance called with parameters for constructor.");
   ok(obj.index !== undefined, "New object has an initial index.");
-  ok(obj.lane_control == undefined, "New object has empty lane_control.");
-  ok(obj.outcome == undefined, "New object has empty outcome.");
+  ok(obj.lane_control === undefined, "New object has empty lane_control.");
+  ok(obj.outcome === undefined, "New object has empty outcome.");
     
   obj = new NPG.QC.RunMQCControl();
   ok(obj !== undefined, 'variable is now an instance of RunMQCControl'); 
