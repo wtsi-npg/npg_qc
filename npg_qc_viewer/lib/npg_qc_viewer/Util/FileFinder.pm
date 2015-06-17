2bda0d2fbe09b5d694b27a40a84da8b18e711c4e
jmtcsngr <jmtc@sanger.ac.uk>
2015-06-17 15:00:02 +0100

    + POD

--- npg_qc_viewer/lib/npg_qc_viewer/Util/FileFinder.pm
+++ npg_qc_viewer/lib/npg_qc_viewer/Util/FileFinder.pm
@@ -128,4 +128,58 @@ sub _test_options_compatibility {
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
+__END__
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Moose
+
+=item Carp
+
+=item English qw{-no_match_vars}
+
+=item File::Spec::Functions qw(catfile)
+
+=item File::Basename
+
+=item Readonly
+
+=item npg_qc::Schema
+
+=back
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 AUTHOR
+
+Marina Gourtovaia E<lt>mg8@sanger.ac.ukE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (C) 2015 Genome Research Ltd.
+
+This file is part of NPG software.
+
+NPG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
 
