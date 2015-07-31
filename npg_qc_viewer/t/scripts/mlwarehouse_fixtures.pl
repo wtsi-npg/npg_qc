7bac40a8f87e8b0c9d82109d6683b564b32ad7b8
jmtcsngr <jmtc@sanger.ac.uk>
2015-07-31 12:11:52 +0100

    Using the export function to get test data directly

--- npg_qc_viewer/t/scripts/mlwarehouse_fixtures.pl
+++ npg_qc_viewer/t/scripts/mlwarehouse_fixtures.pl
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use npg_warehouse::Schema;
+use WTSI::DNAP::Warehouse::Schema;
 use t::util;
 
 my $util = t::util->new();
@@ -9,12 +9,32 @@ my $real = WTSI::DNAP::Warehouse::Schema->connect();
 my $tname;
 my $rs;
 
-############   iseq_flowcell  #######################
-$tname = q[IseqFlowcell];
-my $run_ids = [3500, 3323, 3965, 4025, 1272, 4950];
-$rs = $real->resultset($tname)->search(
-            {id_run => $run_ids}
-);
-$util->rs_list2fixture($tname, [$rs], $path);
+{
+  ############   iseq_product_metrics  #######################
+  my $run_ids = [1272, 3323, 3500, 3965, 4025, 4950];
+  $tname = q[IseqProductMetric];
+  $rs = $real->resultset($tname)->search(
+              { 'id_run' => $run_ids},
+  );
+  $util->rs_list2fixture(q[300-].$tname, [$rs], $path);
+
+  ############   iseq_flowcell  #######################
+  $tname = q[IseqFlowcell];
+  $rs = $real->resultset($tname)->search(
+              { 'iseq_product_metrics.id_run' => $run_ids},
+              {  join                        => ['iseq_product_metrics'],
+              }
+  );
+  $util->rs_list2fixture(q[400-].$tname, [$rs], $path);
+  
+  ############   iseq_run_lane_metrics  #######################
+  $tname = q[IseqRunLaneMetric];
+  $rs = $real->resultset($tname)->search(
+              { 'iseq_product_metrics.id_run' => $run_ids},
+              {  join                        => ['iseq_product_metrics'],
+              }
+  );
+  $util->rs_list2fixture(q[400-].$tname, [$rs], $path);
+}
 
 1;
