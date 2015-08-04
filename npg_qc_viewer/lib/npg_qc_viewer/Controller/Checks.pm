896f7ad86d450f132c9807148855511b5cddc934
jmtcsngr <jmtc@sanger.ac.uk>
2015-08-04 12:29:17 +0100

    Fixing trailing spaces

--- npg_qc_viewer/lib/npg_qc_viewer/Controller/Checks.pm
+++ npg_qc_viewer/lib/npg_qc_viewer/Controller/Checks.pm
@@ -276,7 +276,7 @@ sub _run_lanes_from_dwh {
   my $row_data = {};
   my $model_mlwh = $c->model('MLWarehouseDB');
 
-  $rs = $model_mlwh->search_product_metrics_by_run($where); 
+  $rs = $model_mlwh->search_product_metrics_by_run($where);
 
   $rs = $model_mlwh->resultset('IseqProductMetric')->
                      search($where, {
@@ -306,7 +306,7 @@ sub _run_lanes_from_dwh {
 
         if ( defined $product_metric->iseq_flowcell->sample ) {
           my $sample_row = $product_metric->iseq_flowcell->sample;
-          
+
           #TODO pass through facade
           $values->{'id_sample_lims'} = $sample_row->id_sample_lims;
           $values->{'name'}           = $sample_row->name;
