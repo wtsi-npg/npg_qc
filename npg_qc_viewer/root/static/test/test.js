7fc39d6b54d65ef6010641be33b7baf1ea7c3d36
jmtcsngr <jmtc@sanger.ac.uk>
2015-10-01 14:08:00 +0100

    Adding tests for basic instantiation

--- npg_qc_viewer/root/static/test/test.js
+++ npg_qc_viewer/root/static/test/test.js
@@ -18,12 +18,32 @@ test('Object initialisation', function() {
   obj = new NPG.QC.LaneMQCControl(new TestConfiguration());
   ok(obj !== undefined, "Variable is now a new instance called with parameter for constructor.");
   ok(obj.lane_control == null, "New object has null lane_control.");
+  ok(obj.abstractConfiguration !== undefined, 'Object has a configuration');
   ok(obj.outcome == null, "New object has null outcome.");
 
+  obj = null;
+  ok(obj == null, "Variable back to null.");
+  obj = new NPG.QC.LibraryMQCControl();
+  ok(obj !== undefined, "Variable is now an instance.");
+  obj = new NPG.QC.LibraryMQCControl(new TestConfiguration());
+  ok(obj !== undefined, "Variable is now a new instance called with parameter for constructor.");
+  ok(obj.lane_control == null, "New object has null lane_control.");
+  ok(obj.abstractConfiguration !== undefined, 'Object has a configuration');
+  ok(obj.outcome == null, "New object has null outcome.");
+
+  obj = null;
+  ok(obj == null, "Variable back to null.");
   obj = new NPG.QC.RunPageMQCControl();
   ok(obj !== undefined, 'variable is now an instance of RunPageMQCControl');
   obj = new NPG.QC.RunPageMQCControl(new TestConfiguration());
   ok(obj !== undefined, 'variable is now an instance of RunPageMQCControl');
+
+  obj = null;
+  ok(obj == null, "Variable back to null.");
+  obj = new NPG.QC.LanePageMQCControl();
+  ok(obj !== undefined, 'variable is now an instance of LanePageMQCControl');
+  obj = new NPG.QC.LanePageMQCControl(new TestConfiguration());
+  ok(obj !== undefined, 'variable is now an instance of LanePageMQCControl');
 
   obj = new NPG.QC.RunTitleParser();
   ok(obj !== undefined, 'variable is now an instance of RunTitleParser');
