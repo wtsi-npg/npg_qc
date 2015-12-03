f2101f0b3f22aebe42ac10810ecb99c89074d14f
jmtcsngr <jmtc@sanger.ac.uk>
2015-12-03 10:08:17 +0000

    Moving all display on demand to AMD
    Using factory-like method for building displayOnDemand element

--- npg_qc_viewer/root/static/scripts/display_on_demand.js
+++ npg_qc_viewer/root/static/scripts/display_on_demand.js
@@ -21,10 +21,53 @@ define(['jquery'], function (jQuery) {
     }
 
     return ( (o2s >= o1s && o2s <= o1e) || (o2e >= o1s && o2e <= o1e) );
-  }
+  };
+
+  var buildDisplayOnViewElement = function (selectorFilter, threshold, displayFunction, removeFunction) {
+    var config1 = {
+      selectorFilter  : selectorFilter,
+      threshold       : threshold,
+      displayFunction : displayFunction,
+      removeFunction  : removeFunction
+    };
+    return config1;
+  };
+
+  var displayOnView = function (elements) {
+    $(window).on('scroll resize lookup', function() {
+      for (var j = 0; j < elements.length; j++) {
+        var config = elements[j];
+        var threshold = config.threshold;
+        var $w = $(window);
+        var wt = $w.scrollTop() - threshold;
+        var viewHeight = $w.height() + (2 * threshold);
+
+        $(config.selectorFilter).each(function (i, obj) {
+          var self = $(this);
+          var selfTop = self.offset().top;
+
+          if ( overlap( wt, viewHeight, selfTop, self.height() ) ) {
+            if (self.data('inView') === undefined || self.data('inView') == 0) {
+              window.console.log("Building plots " + i);
+              self.data('inView', 1);
+              config.displayFunction(i, self);
+            }
+          } else {
+            if (self.data('inView') == 1) {
+              window.console.log("Destroying plots " + i);
+              self.data('inView', 0);
+              config.removeFunction(i, self);
+            }
+          }
+        });
+      }
+    });
+  };
 
   return {
     overlap : overlap,
+    buildDisplayOnViewElement : buildDisplayOnViewElement,
+    displayOnView : displayOnView,
   };
 });
 
