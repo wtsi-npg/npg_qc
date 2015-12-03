969e4f4db4f6b62e89dafb33ffc0a31a1946c1ac
jmtcsngr <jmtc@sanger.ac.uk>
2015-12-03 10:32:45 +0000

    Using amd function in tests

--- npg_qc_viewer/root/static/test/test_plot_on_demand.js
+++ npg_qc_viewer/root/static/test/test_plot_on_demand.js
@@ -6,35 +6,35 @@ requirejs.config({
   },
 });
 
-define(['scripts/display_on_demand',], function(plot_on_demand) {
-
-    $(window).on("scroll resize lookup", function () {
-      var self = $(this);
-      var threshold = self.innerHeight() * 0.75;
-      var wt = self.scrollTop() - threshold, wh = ( self.innerHeight() + ( 2 * threshold ) );
-
-      var elements = ['first', 'up', 'middle', 'down', 'last'];
-      for (var i = 0; i < elements.length; i++) {
-        var element = $($('#' + elements[i]).first());
-        var fromTop = element.offset().top;
-        if (plot_on_demand.overlap(wt, wh, fromTop, element.height() )) {
-          if ( element.data('inView') == 0 ) {
-            element.data('inView', 1);
-          }
-        } else {
-          element.data('inView', 0);
-        }
-      }
-    });
-
+define(['scripts/display_on_demand',], function(disp_on_dem) {
     // run the tests.
-    QUnit.test('Testing changing object on window scroll', function (assert) {
+    QUnit.test('Testing changing objects on window scroll', function (assert) {
       var $w = $(window);
-      var $first = $('#first').first(); $first.data('inView', 0);
-      var $up = $('#up').first(); $up.data('inView', 0);
-      var $middle = $('#middle').first(); $middle.data('inView', 0);
-      var $down = $('#down').first(); $down.data('inView', 0);
-      var $last = $('#last').first(); $last.data('inView', 0);
+      var elements = ['first', 'up', 'middle', 'down', 'last'];
+      var threshold = $w.innerHeight() * 0.75;
+      var elementsToDisplay = [];
+
+      for (var i = 0; i < elements.length; i++) {
+        var selectorFilter = '#' + elements[i];
+        $($(selectorFilter).first()).data('displayed', 0);
+
+        var element = disp_on_dem.buildDisplayOnViewElement(
+          selectorFilter,
+          threshold,
+          function (i, obj) { obj.data('displayed', 1) },
+          function (i, obj) { obj.data('displayed', 0) }
+        );
+
+        elementsToDisplay.push(element);
+      }
+
+      disp_on_dem.displayOnView(elementsToDisplay);
+
+      var $first = $('#first').first();
+      var $up = $('#up').first();
+      var $middle = $('#middle').first();
+      var $down = $('#down').first();
+      var $last = $('#last').first();
 
       var windowHeight = $w.innerHeight();
       $(".padding_long").each(function () {
@@ -53,51 +53,51 @@ define(['scripts/display_on_demand',], function(plot_on_demand) {
       var windowStartPos = $first.offset().top - 10;
       window.scroll(0, windowStartPos);
       $w.trigger("scroll");
-      assert.equal($first.data('inView'), 1, "first has inView");
-      assert.equal($up.data('inView'), 0, "up has not inView");
-      assert.equal($middle.data('inView'), 0, "middle has not inView");
-      assert.equal($down.data('inView'), 0, "down has not inView");
-      assert.equal($last.data('inView'), 0, "last has not inView");
+      assert.equal($first.data('displayed'), 1, "first has displayed");
+      assert.equal($up.data('displayed'), 0, "up has not displayed");
+      assert.equal($middle.data('displayed'), 0, "middle has not displayed");
+      assert.equal($down.data('displayed'), 0, "down has not displayed");
+      assert.equal($last.data('displayed'), 0, "last has not displayed");
 
       window.scroll(0, $up.offset().top) - 10;
       $w.trigger("scroll");
-      assert.equal($first.data('inView'), 0, "first has not inView");
-      assert.equal($up.data('inView'), 1, "up has inView");
-      assert.equal($middle.data('inView'), 0, "middle has not inView");
-      assert.equal($down.data('inView'), 0, "down has not inView");
-      assert.equal($last.data('inView'), 0, "last has not inView");
+      assert.equal($first.data('displayed'), 0, "first has not displayed");
+      assert.equal($up.data('displayed'), 1, "up has displayed");
+      assert.equal($middle.data('displayed'), 0, "middle has not displayed");
+      assert.equal($down.data('displayed'), 0, "down has not displayed");
+      assert.equal($last.data('displayed'), 0, "last has not displayed");
 
       window.scroll(0, $middle.offset().top) - 10;
       $w.trigger("scroll");
-      assert.equal($first.data('inView'), 0, "first has not inView");
-      assert.equal($up.data('inView'), 0, "up has not inView");
-      assert.equal($middle.data('inView'), 1, "middle has inView");
-      assert.equal($down.data('inView'), 1, "down has inView");
-      assert.equal($last.data('inView'), 0, "last has not inView");
+      assert.equal($first.data('displayed'), 0, "first has not displayed");
+      assert.equal($up.data('displayed'), 0, "up has not displayed");
+      assert.equal($middle.data('displayed'), 1, "middle has displayed");
+      assert.equal($down.data('displayed'), 1, "down has displayed");
+      assert.equal($last.data('displayed'), 0, "last has not displayed");
 
       window.scroll(0, $down.offset().top) - 10;
       $w.trigger("scroll");
-      assert.equal($first.data('inView'), 0, "first has not inView");
-      assert.equal($up.data('inView'), 0, "up has not inView");
-      assert.equal($middle.data('inView'), 1, "middle has inView");
-      assert.equal($down.data('inView'), 1, "down has inView");
-      assert.equal($last.data('inView'), 1, "last has inView");
+      assert.equal($first.data('displayed'), 0, "first has not displayed");
+      assert.equal($up.data('displayed'), 0, "up has not displayed");
+      assert.equal($middle.data('displayed'), 1, "middle has displayed");
+      assert.equal($down.data('displayed'), 1, "down has displayed");
+      assert.equal($last.data('displayed'), 1, "last has displayed");
 
       window.scroll(0, $up.offset().top) - 10;
       $w.trigger("scroll");
-      assert.equal($first.data('inView'), 0, "first has not inView");
-      assert.equal($up.data('inView'), 1, "up has inView");
-      assert.equal($middle.data('inView'), 0, "middle has not inView");
-      assert.equal($down.data('inView'), 0, "down has not inView");
-      assert.equal($last.data('inView'), 0, "last has not inView");
+      assert.equal($first.data('displayed'), 0, "first has not displayed");
+      assert.equal($up.data('displayed'), 1, "up has displayed");
+      assert.equal($middle.data('displayed'), 0, "middle has not displayed");
+      assert.equal($down.data('displayed'), 0, "down has not displayed");
+      assert.equal($last.data('displayed'), 0, "last has not displayed");
 
       window.scroll(0, $first.offset().top - 10);
       $w.trigger("scroll");
-      assert.equal($first.data('inView'), 1, "first has inView");
-      assert.equal($up.data('inView'), 0, "up has not inView");
-      assert.equal($middle.data('inView'), 0, "middle has not inView");
-      assert.equal($down.data('inView'), 0, "down has not inView");
-      assert.equal($last.data('inView'), 0, "last has not inView");
+      assert.equal($first.data('displayed'), 1, "first has displayed");
+      assert.equal($up.data('displayed'), 0, "up has not displayed");
+      assert.equal($middle.data('displayed'), 0, "middle has not displayed");
+      assert.equal($down.data('displayed'), 0, "down has not displayed");
+      assert.equal($last.data('displayed'), 0, "last has not displayed");
 
     });
     QUnit.start();
