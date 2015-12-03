0befcad3147965e52e1c0afaa97f93b355f26399
jmtcsngr <jmtc@sanger.ac.uk>
2015-12-03 11:28:44 +0000

    Parameter validations and defaults

--- npg_qc_viewer/root/static/scripts/display_on_demand.js
+++ npg_qc_viewer/root/static/scripts/display_on_demand.js
@@ -9,8 +9,20 @@
  *
  */
 define(['jquery'], function (jQuery) {
-
   var overlap = function (start1, height1, start2, height2) {
+    if(typeof(start1) === "undefined" || start1 == null) {
+      throw new TypeError("First element start position can not be undefined");
+    }
+    if(typeof(height1) === "undefined" || height1 == null) {
+      throw new TypeError("First element height can not be undefined");
+    }
+    if(typeof(start2) === "undefined" || start2 == null) {
+      throw new TypeError("Second element start position can not be undefined");
+    }
+    if(typeof(height2) === "undefined" || height2 == null) {
+      throw new TypeError("Second element height can not be undefined");
+    }
+
     var o1s, o1e, o2s, o2e;
     if ( height1 >= height2 ) {
       o1s = start1; o1e = start1 + height1;
@@ -24,19 +36,34 @@ define(['jquery'], function (jQuery) {
   };
 
   var buildDisplayOnViewElement = function (selectorFilter, threshold, displayCallback, removeCallback) {
+    if(typeof(selectorFilter) === "undefined" || selectorFilter == null) {
+      throw new TypeError("selectorFilter can not be undefined");
+    }
+
+    threshold = typeof threshold !== 'undefined' ? threshold : 0;
     displayCallback = typeof displayCallback !== 'undefined' ? displayCallback : function (i, obj) { };
     removeCallback = typeof removeCallback !== 'undefined' ? removeCallback : function (i, obj) { };
+
     var element = {
       selectorFilter  : selectorFilter,
       threshold       : threshold,
       displayCallback : displayCallback,
       removeCallback  : removeCallback
     };
+
     return element;
   };
 
   var displayOnView = function (elements, verbose) {
+    if(typeof(elements) === "undefined" || elements == null) {
+      throw new TypeError("elements can not be undefined");
+    }
+    if( !Array.isArray(elements) ) {
+      throw new TypeError("elements must be an array");
+    }
+
     verbose = typeof verbose !== 'undefined' ?  verbose : false;
+
     $(window).on('scroll resize lookup', function() {
       for (var j = 0; j < elements.length; j++) {
         var element = elements[j];
