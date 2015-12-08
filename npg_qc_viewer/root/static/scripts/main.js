f44182692582c5d26e6fbd03652c0aa70e359e67
jmtcsngr <jmtc@sanger.ac.uk>
2015-12-08 11:43:38 +0000

    Moving plotting to new module

--- npg_qc_viewer/root/static/scripts/main.js
+++ npg_qc_viewer/root/static/scripts/main.js
@@ -4,9 +4,6 @@ require.config({
   paths: {
     jquery: 'bower_components/jquery/dist/jquery',
     d3: 'bower_components/d3/d3.min',
-    insert_size_lib: 'bower_components/bcviz/src/qcjson/insertSizeHistogram',
-    adapter_lib: 'bower_components/bcviz/src/qcjson/adapter',
-    mismatch_lib: 'bower_components/bcviz/src/qcjson/mismatch',
     unveil: 'bower_components/jquery-unveil/jquery.unveil',
     'table-export': 'bower_components/table-export/tableExport.min',
   },
@@ -26,16 +23,14 @@ require.onError = function (err) {
     throw err;
 };
 
-function _getTitle(prefix, d) {
-    var t = prefix;
-    if (d.id_run) { t = t + 'run ' + d.id_run; }
-    if (d.position) { t = t + ', lane ' + d.position; }
-    if (d.tag_index) { t = t + ', tag ' + d.tag_index; }
-    return t;
-}
-
-require( ['scripts/manual_qc', 'scripts/manual_qc_ui', 'scripts/format_for_csv', 'scripts/modify_on_view', 'insert_size_lib', 'adapter_lib', 'mismatch_lib', 'unveil', 'table-export'],
-function( manual_qc, manual_qc_ui, format_for_csv, mov, insert_size, adapter, mismatch, unveil) {
+require([
+  'scripts/manual_qc',
+  'scripts/manual_qc_ui',
+  'scripts/plots',
+  'scripts/format_for_csv',
+  'unveil'
+],
+function( manual_qc, manual_qc_ui, plots, format_for_csv, unveil) {
   //Setup for heatmaps to load on demand.
   $(document).ready(function(){
     $("img").unveil(2000);
@@ -84,103 +79,6 @@ function( manual_qc, manual_qc_ui, format_for_csv, mov, insert_size, adapter, mi
     formated_table.tableExport({type:'csv', fileName:'summary_data'});
   });
 
-  var element = mov.buildModifyOnViewElement(
-      '.results_full_lane_contents', //Selector
-      2000, // Threshold
-      function (i, obj) { //Display call back
-        obj.find('.bcviz_insert_size').each(function () {
-          var self = $(this);
-          if ( self.children().length === 0 ) {
-            var parent = self.parent();
-            var d = $.extend( true, {}, self.data('check') ),
-                w = self.data('width') || 650,
-                h = self.data('height') || 300,
-                t = self.data('title') || _getTitle('Insert Sizes : ',d);
-            var chart = insert_size.drawChart({'data': d, 'width': w, 'height': h, 'title': t});
-
-            if (chart != null) {
-              if (chart.svg != null) {
-                var div = $(document.createElement("div"));
-                div.append(function() { return chart.svg.node(); } );
-                div.addClass('chart');
-                self.append(div);
-                div = null;
-              }
-            }
-            parent.css('min-height', parent.height() + 'px');
-            //Nulling variables to ease GC
-            d = null, w = null, h = null, t = null, parent = null, self = null;
-          }
-        });
-        obj.find('.bcviz_adapter').each(function () {
-          var self = $(this);
-          if ( self.children().length === 0 ) {
-            var parent = self.parent();
-            var d = $.extend( true, {}, self.data('check') ),
-                h = self.data('height') || 200,
-                t = self.data('title') || _getTitle('Adapter Start Count : ', d);
-
-            // override width to ensure two graphs can fit side by side
-            var w = jQuery(this).parent().width() / 2 - 40;
-            var chart = adapter.drawChart({'data': d, 'width': w, 'height': h, 'title': t});
-
-            var fwd_div = $(document.createElement("div"));
-            if (chart != null && chart.svg_fwd != null) { fwd_div.append( function() { return chart.svg_fwd.node(); } ); }
-            fwd_div.addClass('chart_left');
-            rev_div = $(document.createElement("div"));
-            if (chart != null && chart.svg_rev != null) { rev_div.append( function() { return chart.svg_rev.node(); } ); }
-            rev_div.addClass('chart_right');
-            self.append(fwd_div,rev_div);
-            parent.css('min-height', parent.height() + 'px');
-            //Nulling variables to ease GC
-            d = null; w = null,  h = null; t = null, chart = null, fwd_div = null, rev_div = null, parent = null, self = null;
-          }
-        });
-        obj.find('.bcviz_mismatch').each(function () {
-          var self = $(this);
-          if ( self.children().length === 0 ) {
-            var parent = self.parent();
-            var d = $.extend( true, {}, self.data('check') ),
-                h = self.data('height'),
-                t = self.data('title') || _getTitle('Mismatch : ', d);
-
-            // override width to ensure two graphs can fit side by side
-            var w = parent.width() / 2 - 90;
-            var chart = mismatch.drawChart({'data': d, 'width': w, 'height': h, 'title': t});
-
-            var fwd_div = $(document.createElement("div"));
-            if (chart != null && chart.svg_fwd != null) { fwd_div.append( function() { return chart.svg_fwd.node(); } ); }
-            fwd_div.addClass('chart_left');
-
-            var rev_div = $(document.createElement("div"));
-            if (chart != null && chart.svg_rev != null) { rev_div.append( function() { return chart.svg_rev.node(); } ); }
-            rev_div.addClass('chart_right');
-
-            var leg_div = $(document.createElement("div"));
-            if (chart != null && chart.svg_legend != null) { leg_div.append( function() { return chart.svg_legend.node(); } ); }
-            leg_div.addClass('chart_legend');
-
-            self.append(fwd_div,rev_div,leg_div);
-            parent.css('min-height', parent.height() + 'px');
-            //Nulling variables to ease GC
-            d = null; w = null,  h = null; t = null, chart = null, fwd_div = null, rev_div = null, leg_div = null, parent = null, self = null;
-          }
-        });
-      },
-      function (i, obj) { // remove callback
-        if (obj.find('.bcviz_insert_size').children().length > 0) {
-          obj.find('.bcviz_insert_size').empty();
-        }
-        if (obj.find('.bcviz_adapter').children().length > 0) {
-          obj.find('.bcviz_adapter').empty();
-        }
-        if (obj.find('.bcviz_mismatch').children().length > 0) {
-          obj.find('.bcviz_mismatch').empty();
-        }
-      }
-  );
-
-  var elements = [element];
-  mov.modifyOnView(elements, true);
+  plots.plots_on_view('.results_full_lane_contents', 2000); 
 });
 
