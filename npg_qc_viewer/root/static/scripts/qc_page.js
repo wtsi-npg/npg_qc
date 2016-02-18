fba80b899b0b38a04440579da28d4ccca1370a95
jmtcsngr <jmtc@sanger.ac.uk>
2016-02-18 10:26:37 +0000

    Returning if page is single run/lane

--- npg_qc_viewer/root/static/scripts/qc_page.js
+++ npg_qc_viewer/root/static/scripts/qc_page.js
@@ -5,7 +5,7 @@
 define(['jquery'], function (jQuery) {
   var _reRunLane   = /(?:(?:for (run) ([0-9]+))|(?:for (runs) ([0-9]+) lanes ([0-9]+))) \(run/;
   var _reRunStatus = /\(run [0-9]+ status: ((?:[\S]+)(?:\s[\S]+){2}), taken by ([\S]+)\)$/;
-  
+
   var _reLoggedUser = /^Logged in as ([a-zA-Z0-9]+)(?:[\s]{1}(?:\((mqc)\)))?$/i; // [1] username, [2] (mqc)
 
   var _parseRunLane = function(titleString) {
@@ -14,23 +14,26 @@ define(['jquery'], function (jQuery) {
     }
 
     var matchRunLane = _reRunLane.exec(titleString);
-    var isRunPage = null;
+    var isRunPage = null, isSingleRunOrSingleLanePage = false;
 
     if ( matchRunLane != null ) {
       if( matchRunLane.constructor === Array && matchRunLane.length > 1 ) {
         if ( typeof matchRunLane[1] !== 'undefined' ) {
           isRunPage = true;
+          isSingleRunOrSingleLanePage = true;
         } else if ( typeof matchRunLane[3] !== 'undefined' ) {
           isRunPage = false;
+          isSingleRunOrSingleLanePage = true;
         }
       }
     }
 
     return {
-      isRunPage: isRunPage
+      isRunPage: isRunPage,
+      isSingleRunOrSingleLanePage: isSingleRunOrSingleLanePage
     };
   };
-  
+
   var _parseRunStatus = function(titleString) {
     if ( typeof titleString !== 'string' ) {
       throw new Error('Invalid arguments');
@@ -57,7 +60,7 @@ define(['jquery'], function (jQuery) {
       throw new Error('Invalid arguments');
     }
     var username = null, role = null;
-    
+
     var matchLoggedUser = _reLoggedUser.exec(loggedUserString);
     if (matchLoggedUser != null) {
       if (matchLoggedUser.constructor === Array && matchLoggedUser.length > 1) {
@@ -67,16 +70,16 @@ define(['jquery'], function (jQuery) {
         }
       }
     }
-    
+
     return {
-      username: username, //
-      role:     role //
+      username: username,
+      role:     role
     };
   };
 
   var pageForMQC = function() {
     var isPageForMQC = false, isRunPage = null;
-    
+
     var loggedUserString = $('#header h1 span.rfloat').text();
     var loggedUserData = _parseLoggedUser(loggedUserString);
     if ( loggedUserData.username != null &&
@@ -87,12 +90,14 @@ define(['jquery'], function (jQuery) {
            ( runStatusData.runStatus === 'qc in progress' ||
              runStatusData.runStatus === 'qc on hold') &&
            loggedUserData.username === runStatusData.takenBy ) {
-        isPageForMQC = true;
         var runInfo = _parseRunLane(pageTitleString);
         isRunPage = runInfo.isRunPage;
+        if ( runInfo.isSingleRunOrSingleLanePage ) {
+          isPageForMQC = true;
+        }
       }
     }
-    
+
     return {
       isPageForMQC: isPageForMQC,
       isRunPage:   isRunPage
