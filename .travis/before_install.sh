a46b66949a8f706dc76225156013aaa8e87b6fb1
jmtcsngr <jmtc@sanger.ac.uk>
2016-04-07 17:59:44 +0100

    moving node to 4 in travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,7 +5,9 @@ set -e -x
 sudo apt-get update -qq
 mysql -e "CREATE DATABASE npgqct;" -uroot 
 
-npm install -g bower
+rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (pushd ~/.nvm && git checkout v0.31.0 && popd) && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION
+
+npm install -g bower@1.7.9
 npm install -g node-qunit-phantomjs
 
 # Dummy executable files generated for tests use #!/usr/local/bin/bash
--- .travis/script.sh
+++ .travis/script.sh
@@ -4,6 +4,8 @@ set -e -x
 
 unset PERL5LIB
 
+export PATH=/home/travis/.nvm/versions/node/v${TRAVIS_NODE_VERSION}/bin:$PATH
+
 cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
 perl Build.PL
 ./Build
