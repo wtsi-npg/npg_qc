75d438e3ea96c08b5e69175d3ddf898e8c6ae6f6
jmtcsngr <jmtc@sanger.ac.uk>
2016-05-31 16:54:12 +0100

    Update README and remove comments from Gruntfile

--- npg_qc_viewer/Gruntfile.js
+++ npg_qc_viewer/Gruntfile.js
@@ -11,16 +11,7 @@ module.exports = function(grunt) {
     jscs: {
       main: [
         'Gruntfile.js',
-/*        'root/static/scripts/main.js',
-        'root/static/scripts/qcoutcomes/manual_qc.js',
-        'root/static/scripts/qcoutcomes/manual_qc_ui.js',
-        'root/static/scripts/modify_on_view.js',
-        'root/static/scripts/plots.js',
-        'root/static/scripts/qcoutcomes/qc_css_styles.js',
-        'root/static/scripts/qcoutcomes/qc_outcomes_view.js',
-        'root/static/scripts/qcoutcomes/qc_page.js',
-        'root/static/scripts/qcoutcomes/qc_utils.js',
-        't/client/test*.js'*/
+        'root/static/scripts/collapse.js',
       ],
       options: {
         config: '.jscsrc'
@@ -29,18 +20,7 @@ module.exports = function(grunt) {
     jshint: {
       all: [
         'Gruntfile.js',
-/*        'root/static/scripts/main.js',
-        'root/static/scripts/qcoutcomes/manual_qc.js',
-        'root/static/scripts/qcoutcomes/manual_qc_ui.js',
-        'root/static/scripts/modify_on_view.js',
-        'root/static/scripts/plots.js',
-        'root/static/scripts/qcoutcomes/qc_css_styles.js',
-        'root/static/scripts/qcoutcomes/qc_outcomes_view.js',
-        'root/static/scripts/qcoutcomes/qc_page.js',
-        'root/static/scripts/qcoutcomes/qc_utils.js',*/
-
-        //'root/static/scripts/**/*.js',
-        //TESTS
+        'root/static/scripts/collapse.js',
         't/client/test*.js'
       ],
       options: {
