a6304d0c535c802b4c41a7ad7878cb59a63e6d55
jmtcsngr <jmtc@sanger.ac.uk>
2016-06-14 16:08:51 +0100

    split into two scripts

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,31 +2,9 @@
 
 set -e -x
 
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
-sudo apt-get remove --purge "^mysql.*"
-sudo apt-get autoremove
-sudo apt-get autoclean
-
 sudo apt-get update -qq
 
-sudo rm -rf /var/lib/mysql
-sudo rm -rf /var/log/mysql
-
-echo "deb http://repo.mysql.com/apt/ubuntu/ precise mysql-5.7" | sudo tee --append /etc/apt/sources.list.d/mysql.list
-sudo apt-get update -q
-#MYSQL_PASS="777"
-#echo mysql-server mysql-server/root_password password "${MYSQL_PASS}" | sudo debconf-set-selections
-#echo mysql-server mysql-server/root_password_again password "${MYSQL_PASS}" | sudo debconf-set-selections
-sudo -E apt-get install -q -y mysql-server libmysqlclient18
-echo $PATH
-ls -l $(dirname $(which mysqld))
-
-sudo /etc/init.d/mysql stop
-sudo mysqld --skip-grant-tables &
-
-#echo "dbpass=${MYSQL_PASS}" | tee -a  npg_qc/data/npg_qc_web/config.ini
-
-sleep 20 && mysql -e "CREATE DATABASE npgqct;" -uroot
+mysql -e "CREATE DATABASE npgqct;" -uroot
 
 # shellcheck source=/dev/null
 rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (pushd ~/.nvm && git checkout v0.31.0 && popd) && source ~/.nvm/nvm.sh && nvm install "${TRAVIS_NODE_VERSION}"
