326bdc76bcc242485b67585cb4dfb208e58c8454
iainrb <ib5@sanger.ac.uk>
2016-09-16 14:19:17 +0100

    Add the file of iRODS setup responses

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -12,7 +12,7 @@ before_script_4_1_x() {
     sudo -E -u postgres createuser -D -R -S irods
     sudo -E -u postgres createdb -O irods ICAT
     sudo -E -u postgres sh -c "echo \"ALTER USER irods WITH PASSWORD 'irods'\" | psql"
-    sudo /var/lib/irods/packaging/setup_irods.sh < .travis.setup_irods
+    sudo /var/lib/irods/packaging/setup_irods.sh < setup_irods
     sudo jq -f .travis.server_config /etc/irods/server_config.json > server_config.tmp
     sudo mv server_config.tmp /etc/irods/server_config.json
     ls -l /etc/irods
