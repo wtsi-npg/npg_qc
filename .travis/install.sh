4ce62bd01ca2ead0cce13e9655b565abb468dca9
Marina Gourtovaia <mg8@sanger.ac.uk>
2016-11-24 16:47:43 +0000

    beter version

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,6 +7,8 @@
 
 set -e -x
 
+WTSI_NPG_BUILD_BRANCH=${WTSI_NPG_BUILD_BRANCH:=$TRAVIS_BRANCH}
+
 sudo apt-get install libgd2-xpm-dev # For npg_tracking
 sudo apt-get install liblzma-dev # For npg_qc
 sudo apt-get install --yes nodejs
@@ -19,16 +21,17 @@ cpanm --quiet --notest Alien::Tidyp
 cpanm --no-lwp --notest https://github.com/wtsi-npg/perl-dnap-utilities/releases/download/${DNAP_UTILITIES_VERSION}/WTSI-DNAP-Utilities-${DNAP_UTILITIES_VERSION}.tar.gz
 
 # WTSI NPG Perl repo dependencies
-cd /tmp
-git clone --branch ${TRAVIS_BRANCH} --depth 1 https://github.com/wtsi-npg/ml_warehouse.git ml_warehouse.git
-git clone --branch ${TRAVIS_BRANCH} --depth 1 https://github.com/wtsi-npg/npg_tracking.git npg_tracking.git
-git clone --branch ${TRAVIS_BRANCH} --depth 1 https://github.com/wtsi-npg/npg_seq_common.git npg_seq_common.git
-
-repos="/tmp/ml_warehouse.git /tmp/npg_tracking.git /tmp/npg_seq_common.git"
-
+repos="ml_warehouse npg_tracking npg_seq_common"
 for repo in $repos
 do
-  cd "$repo"
+  # Logic of keeping branch consistent was taken from @dkj
+  # contribution to https://github.com/wtsi-npg/npg_irods
+  cd /tmp
+  # Always clone master when using depth 1 to get current tag
+  git clone --branch master --depth 1 ${WTSI_NPG_GITHUB_URL}/${repo}.git ${repo}.git
+  cd /tmp/${repo}.git
+  # Shift off master to appropriate branch (if possible)
+  git ls-remote --heads --exit-code origin ${WTSI_NPG_BUILD_BRANCH} && git pull origin ${WTSI_NPG_BUILD_BRANCH} && echo "Switched to branch ${WTSI_NPG_BUILD_BRANCH}"
   cpanm --quiet --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
   perl Build.PL
   ./Build
