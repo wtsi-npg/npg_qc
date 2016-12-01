12ab4e87506ad98355f99597ff3ffcbfa9c2cc34
iainrb <ib5@sanger.ac.uk>
2016-12-01 16:07:01 +0000

    Modify to use disposable-irods in Travis build

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,101 +10,70 @@
 
 set -e -x
 
-IRODS_VERSION=${IRODS_VERSION:=4.1.9}
+# The default build branch for all repositories. This defaults to
+# TRAVIS_BRANCH unless set in the Travis build environment.
+WTSI_NPG_BUILD_BRANCH=${WTSI_NPG_BUILD_BRANCH:=$TRAVIS_BRANCH}
 
-install_common() {
+sudo apt-get install -qq odbc-postgresql unixodbc-dev
+sudo apt-get install libgd2-xpm-dev # For npg_tracking
+sudo apt-get install liblzma-dev # For npg_qc
+sudo apt-get install --yes nodejs
 
-    sudo apt-get install libgd2-xpm-dev # For npg_tracking
-    sudo apt-get install liblzma-dev # For npg_qc
-    sudo apt-get install --yes nodejs
+# CPAN as in npg_npg_deploy
+cpanm --notest --reinstall App::cpanminus
+cpanm --quiet --notest --reinstall ExtUtils::ParseXS
+cpanm --quiet --notest --reinstall MooseX::Role::Parameterized
+cpanm --quiet --notest Alien::Tidyp
 
-    # CPAN as in npg_npg_deploy
-    cpanm --notest --reinstall App::cpanminus
-    cpanm --quiet --notest --reinstall ExtUtils::ParseXS
-    cpanm --quiet --notest --reinstall MooseX::Role::Parameterized
-    cpanm --quiet --notest Alien::Tidyp
-    cpanm --no-lwp --notest https://github.com/wtsi-npg/perl-dnap-utilities/releases/download/${DNAP_UTILITIES_VERSION}/WTSI-DNAP-Utilities-${DNAP_UTILITIES_VERSION}.tar.gz
+# iRODS
+wget -q https://github.com/wtsi-npg/disposable-irods/releases/download/${DISPOSABLE_IRODS_VERSION}/disposable-irods-${DISPOSABLE_IRODS_VERSION}.tar.gz -O /tmp/disposable-irods-${DISPOSABLE_IRODS_VERSION}.tar.gz
+tar xfz /tmp/disposable-irods-${DISPOSABLE_IRODS_VERSION}.tar.gz -C /tmp
+cd /tmp/disposable-irods-${DISPOSABLE_IRODS_VERSION}
+./scripts/download_and_verify_irods.sh
+./scripts/install_irods.sh
+./scripts/configure_irods.sh
 
-    # WTSI NPG Perl repo dependencies
-    cd /tmp
-    git clone --branch devel --depth 1 https://github.com/wtsi-npg/ml_warehouse.git ml_warehouse.git
-    #git clone --branch devel --depth 1 https://github.com/wtsi-npg/npg_tracking.git npg_tracking.git
-    git clone --branch lims4composition --depth 1 https://github.com/mgcam/npg_tracking.git npg_tracking.git
-    git clone --branch devel --depth 1 https://github.com/wtsi-npg/npg_seq_common.git npg_seq_common.git
-    git clone --branch devel --depth 1 https://github.com/wtsi-npg/perl-irods-wrap.git perl-irods-wrap.git
+# Jansson
+wget -q https://github.com/akheron/jansson/archive/v${JANSSON_VERSION}.tar.gz -O /tmp/jansson-${JANSSON_VERSION}.tar.gz
+tar xfz /tmp/jansson-${JANSSON_VERSION}.tar.gz -C /tmp
+cd /tmp/jansson-${JANSSON_VERSION}
+autoreconf -fi
+./configure ; make ; sudo make install
+sudo ldconfig
 
-    repos="/tmp/ml_warehouse.git /tmp/npg_tracking.git /tmp/npg_seq_common.git /tmp/perl-irods-wrap.git"
+# baton
+wget -q https://github.com/wtsi-npg/baton/releases/download/${BATON_VERSION}/baton-${BATON_VERSION}.tar.gz -O /tmp/baton-${BATON_VERSION}.tar.gz
+tar xfz /tmp/baton-${BATON_VERSION}.tar.gz -C /tmp
+cd /tmp/baton-${BATON_VERSION}
 
-    for repo in $repos
-    do
-        cd $repo
-        cpanm --quiet --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
-        perl Build.PL
-        ./Build
-        ./Build install
-    done
-    
-    cd $TRAVIS_BUILD_DIR
-    sudo ldconfig
-}
+IRODS_HOME=
+baton_irods_conf="--with-irods"
 
-install_4_1_x() {
-    # common to irods 3.3.1 and 4.1.x
-    sudo apt-get install -qq odbc-postgresql unixodbc-dev
-    sudo apt-get install -qq python-sphinx
-    sudo -H pip install breathe
+if [ -n "$IRODS_RIP_DIR" ]
+then
+    export IRODS_HOME="$IRODS_RIP_DIR/iRODS"
+    baton_irods_conf="--with-irods=$IRODS_HOME"
+fi
 
-    wget http://downloads.sourceforge.net/project/check/check/0.9.14/check-0.9.14.tar.gz -O /tmp/check-0.9.14.tar.gz
-    tar xfz /tmp/check-0.9.14.tar.gz -C /tmp
-    cd /tmp/check-0.9.14
-    autoreconf -fi
-    ./configure ; make ; sudo make install
+./configure ${baton_irods_conf} ; make ; sudo make install
+sudo ldconfig
 
-    # Jansson
-    wget -q https://github.com/akheron/jansson/archive/v${JANSSON_VERSION}.tar.gz -O /tmp/jansson-${JANSSON_VERSION}.tar.gz
-    tar xfz /tmp/jansson-${JANSSON_VERSION}.tar.gz -C /tmp
-    cd /tmp/jansson-${JANSSON_VERSION}
-    autoreconf -fi
-    ./configure ; make ; sudo make install
-    sudo ldconfig
+# WTSI NPG Perl repo dependencies
+repos="perl-dnap-utilities ml_warehouse npg_tracking npg_seq_common"
+for repo in $repos
+do
+  # Logic of keeping branch consistent was taken from @dkj
+  # contribution to https://github.com/wtsi-npg/npg_irods
+  cd /tmp
+  # Always clone master when using depth 1 to get current tag
+  git clone --branch master --depth 1 ${WTSI_NPG_GITHUB_URL}/${repo}.git ${repo}.git
+  cd /tmp/${repo}.git
+  # Shift off master to appropriate branch (if possible)
+  git ls-remote --heads --exit-code origin ${WTSI_NPG_BUILD_BRANCH} && git pull origin ${WTSI_NPG_BUILD_BRANCH} && echo "Switched to branch ${WTSI_NPG_BUILD_BRANCH}"
+  cpanm --quiet --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+  perl Build.PL
+  ./Build
+  ./Build install
+done
 
-    cd $TRAVIS_BUILD_DIR
-    sudo ldconfig
-
-    # introduced for irods 4.1.x
-    sudo apt-get install -qq python-psutil python-requests
-    sudo apt-get install -qq python-sphinx
-    sudo apt-get install super libjson-perl jq
-    sudo -H pip install jsonschema
-
-    sudo dpkg --contents irods-icat-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb
-    sudo dpkg -i irods-icat-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb irods-database-plugin-postgres-${PG_PLUGIN_VERSION}-${PLATFORM}-${ARCH}.deb
-    sudo dpkg --contents irods-runtime-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb
-    sudo dpkg -i irods-runtime-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb irods-dev-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb
-}
-
-install_baton() {
-
-    wget -q https://github.com/wtsi-npg/baton/releases/download/${BATON_VERSION}/baton-${BATON_VERSION}.tar.gz -O /tmp/baton-${BATON_VERSION}.tar.gz
-    tar xfz /tmp/baton-${BATON_VERSION}.tar.gz -C /tmp
-    cd /tmp/baton-${BATON_VERSION}
-    ./configure --with-irods
-    make
-    sudo make install
-    cd $TRAVIS_BUILD_DIR
-    sudo ldconfig
-
-}
-
-case $IRODS_VERSION in
-
-    4.1.9)
-        install_common
-        install_4_1_x
-        install_baton
-        ;;
-
-    *)
-        echo Unknown iRODS version $IRODS_VERSION
-        exit 1
-esac
+cd "$TRAVIS_BUILD_DIR"
--- .travis/script.sh
+++ .travis/script.sh
@@ -9,6 +9,9 @@ script_common() {
     unset PERL5LIB
 
     export PATH=/home/travis/.nvm/versions/node/v${TRAVIS_NODE_VERSION}/bin:$PATH
+    export WTSI_NPG_iRODS_Test_irodsEnvFile=$HOME/.irods/.irodsEnv
+    export WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE=$HOME/.irods/irods_environment.json
+    export WTSI_NPG_iRODS_Test_Resource=testResc
 
     cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
     perl Build.PL
