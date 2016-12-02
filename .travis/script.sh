c540f64571a2985f70ab6d93cc7c8de6c8cb53d0
iainrb <ib5@sanger.ac.uk>
2016-12-02 15:16:48 +0000

    First draft of changes to use disposable_irods in tests

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,44 +2,26 @@
 
 set -e -x
 
-IRODS_VERSION=${IRODS_VERSION:=4.1.9}
+unset PERL5LIB
 
-script_common() {
+export PATH=/home/travis/.nvm/versions/node/v${TRAVIS_NODE_VERSION}/bin:$PATH
+export TEST_AUTHOR=1
+export WTSI_NPG_iRODS_Test_irodsEnvFile=$HOME/.irods/.irodsEnv
+export WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE=$HOME/.irods/irods_environment.json
+export WTSI_NPG_iRODS_Test_Resource=testResc
 
-    unset PERL5LIB
+cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+perl Build.PL
+./Build
 
-    export PATH=/home/travis/.nvm/versions/node/v${TRAVIS_NODE_VERSION}/bin:$PATH
+pushd npg_qc_viewer
+cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
+perl Build.PL --installjsdeps
+./Build
+popd
 
-    cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
-    perl Build.PL
-    ./Build
-
-    pushd npg_qc_viewer
-    cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
-    perl Build.PL --installjsdeps
-    ./Build
-    popd
-
-    ./Build test --verbose
-    pushd npg_qc_viewer
-    ./Build test --verbose
-    grunt -v
-    popd
-}
-
-script_4_1_x() {
-    return
-}
-
-
-case $IRODS_VERSION in
-
-    4.1.9)
-        script_common
-        script_4_1_x
-        ;;
-
-    *)
-        echo Unknown iRODS version $IRODS_VERSION
-        exit 1
-esac
+./Build test --verbose
+pushd npg_qc_viewer
+./Build test --verbose
+grunt -v
+popd
