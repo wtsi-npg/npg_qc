bc7b161cc4bd2cb1e073d4dd52d479abc00b14bd
iainrb <ib5@sanger.ac.uk>
2016-12-05 11:39:39 +0000

    Use disposable-irods in Travis build; use perl-irods-wrap

--- .travis/script.sh
+++ .travis/script.sh
@@ -5,6 +5,10 @@ set -e -x
 unset PERL5LIB
 
 export PATH=/home/travis/.nvm/versions/node/v${TRAVIS_NODE_VERSION}/bin:$PATH
+export TEST_AUTHOR=1
+export WTSI_NPG_iRODS_Test_irodsEnvFile=$HOME/.irods/.irodsEnv
+export WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE=$HOME/.irods/irods_environment.json
+export WTSI_NPG_iRODS_Test_Resource=testResc
 
 cpanm --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;
 perl Build.PL
