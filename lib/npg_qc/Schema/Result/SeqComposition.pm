192ae09519d800b903328a959472aeefca5f240f
mc23NPD <mc23@sanger.ac.uk>
2017-09-14 11:11:16 +0100

    DBIx bind btwn uqcTables and seqComposition class

--- lib/npg_qc/Schema/Result/SeqComposition.pm
+++ lib/npg_qc/Schema/Result/SeqComposition.pm
@@ -468,6 +468,36 @@ __PACKAGE__->might_have(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+=head2 uqc_outcome_ent
+
+Type: might_have
+
+Related object: L<npg_qc::Schema::Result::UqcOutcomeEnt>
+
+=cut
+
+__PACKAGE__->might_have(
+  'uqc_outcome_ent',
+  'npg_qc::Schema::Result::UqcOutcomeEnt',
+  { 'foreign.id_seq_composition' => 'self.id_seq_composition' },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+=head2 uqc_outcome_hist
+
+Type: might_have
+
+Related object: L<npg_qc::Schema::Result::UqcOutcomeHist>
+
+=cut
+
+__PACKAGE__->might_have(
+  'uqc_outcome_hist',
+  'npg_qc::Schema::Result::UqcOutcomeHist',
+  { 'foreign.id_seq_composition' => 'self.id_seq_composition' },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 =head2 verify_bam_id
 
 Type: might_have
@@ -484,8 +514,8 @@ __PACKAGE__->might_have(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07047 @ 2017-08-03 12:42:13
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:XvUVRxvQlw5tvrVm6VXeCg
+# Created by DBIx::Class::Schema::Loader v0.07047 @ 2017-09-14 10:42:41
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:8spVcL5KsuN+kD7xs4qx7w
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
