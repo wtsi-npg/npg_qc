e90dd7a333d0907ce740e10b8b62eaa583a5c121
Ruben E. Bautista <rb11@sanger.ac.uk>
2018-03-09 14:57:28 +0000

    Update rna_seq result DBIx schema object; move role tests over; update change log

--- lib/npg_qc/autoqc/results/rna_seqc.pm
+++ lib/npg_qc/autoqc/results/rna_seqc.pm
@@ -177,6 +177,8 @@ All remaining RNA-SeQC metrics as a key-values pairs
 
 =item npg_qc::autoqc::results::result
 
+=item Readonly
+
 =back
 
 =head1 INCOMPATIBILITIES
@@ -189,7 +191,7 @@ Ruben E Bautista-Garcia<lt>rb11@sanger.ac.uk<gt>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2016 Genome Research Limited
+Copyright (C) 2018 Genome Research Limited
 
 This file is part of NPG.
 
