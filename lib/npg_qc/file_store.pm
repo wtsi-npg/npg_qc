9374777adedb6f0559a849c49197aeb70c3bfdd8
Marina Gourtovaia <mg8@sanger.ac.uk>
2018-08-14 18:16:28 +0100

    code simplification; a test to ensure that non-existing directory in input   does not break the script

--- lib/npg_qc/file_store.pm
+++ lib/npg_qc/file_store.pm
@@ -1,16 +1,9 @@
-#########
-# Author:        Marina Gourtovaia
-# Created:       9 August 2010
-#
-
 package npg_qc::file_store;
 
-use strict;
-use warnings;
 use Moose;
 use namespace::autoclean;
 use Carp;
-use English qw{-no_match_vars};
+use Try::Tiny;
 use File::Spec;
 use Perl6::Slurp;
 use File::Basename;
@@ -24,31 +17,29 @@ with 'MooseX::Getopt';
 
 our $VERSION = '0';
 
-Readonly::Scalar my $DEFAULT_EXTENSION   => q[.fastqcheck];
-Readonly::Scalar our $TABLE_NAME         => q[Fastqcheck];
-Readonly::Hash our %THRESHOLDS_DB => ( 20 => 'twenty', 25 => 'twentyfive', 30 => 'thirty', 35 => 'thirtyfive', 40 => 'forty',);
-##no critic (Variables::ProhibitPunctuationVars RequireExtendedFormatting ProhibitEnumeratedClasses)
-my $FILE_NAME_REG_EXP = qr/^(\d+)_(\d)(_\d|_t)?(_[a-z]+)?(#\d+)?\.fastqcheck$/sm;
-##use critic
+Readonly::Scalar my $DEFAULT_EXTENSION => q[.fastqcheck];
+Readonly::Scalar my $TABLE_NAME        => q[Fastqcheck];
+Readonly::Hash   my %THRESHOLDS_DB     => ( 20 => 'twenty',
+                                            25 => 'twentyfive',
+                                            30 => 'thirty',
+                                            35 => 'thirtyfive',
+                                            40 => 'forty', );
 
-has 'path'            => (  isa         => 'ArrayRef',
-                            is          => 'rw',
-                            required    => 1,
+my $FILE_NAME_REG_EXP = qr/\A(\d+)_(\d)(_\d|_t)?[.]fastqcheck\Z/smx;
+
+has 'path'            => (  isa           => 'ArrayRef',
+                            is            => 'ro',
+                            required      => 1,
                             documentation =>
   'a path to a directory with the files to be saved; multiple paths (an array) are accepted',
                          );
 
 has 'schema' =>    ( isa        => 'npg_qc::Schema',
+                     metaclass  => 'NoGetopt',
                      is         => 'ro',
                      required   => 0,
                      lazy_build => 1,
                    );
-
-has 'table_name' => ( isa        => 'Str',
-                      is         => 'ro',
-                      required   => 0,
-                      default    => $TABLE_NAME,
-                    );
 
 sub _build_schema {
   my $self = shift;
@@ -56,14 +47,10 @@ sub _build_schema {
 }
 
 sub _get_files {
-  my ($self) = @_;
-
+  my $self = shift;
   my @all_files = ();
   foreach my $path (@{$self->path}) {
-    my @files = glob File::Spec->catfile($path, q[*] . $DEFAULT_EXTENSION);
-    if (@files) {
-      push @all_files, @files;
-    }
+    push @all_files, (glob File::Spec->catfile($path, q[*] . $DEFAULT_EXTENSION));
   }
   return \@all_files;
 }
@@ -80,64 +67,50 @@ sub _qvalues {
   return;
 }
 
-sub fname2ids {
+sub _fname2ids {
   my $fname = shift;
 
-  my ($id_run, $position, $read, $split, $tag_index) = $fname =~ $FILE_NAME_REG_EXP;
-  if (!$id_run) {
-    croak "Cannot infer id_run from filename $fname";
-  }
-  if (!$position) {
-    croak "Cannot infer position from filename $fname";
-  }
-
-  if (!$read || $read eq '_1') {
-    $read = 'forward';
-  } elsif ($read eq '_2') {
-    $read = 'reverse';
-  } elsif ($read eq '_t') {
-    $read = 'index';
-  } else {
-    croak "Unknown read type $read in $fname";
+  my ($id_run, $position, $read) = $fname =~ $FILE_NAME_REG_EXP;
+  if ($id_run && $position) {
+    if (!$read || $read eq '_1') {
+      $read = 'forward';
+    } elsif ($read eq '_2') {
+      $read = 'reverse';
+    } elsif ($read eq '_t') {
+      $read = 'index';
+    } else {
+      croak "Unknown read type $read in $fname";
+    }
+    return {'id_run' => $id_run, 'position' => $position, 'section' => $read};
   }
 
-  my $ids = {id_run => $id_run, position => $position, section => $read,};
-  if (defined $tag_index) {
-    $tag_index =~ s/^\#//smx;
-    $ids->{tag_index} = $tag_index;
-  }
-  if ($split) {
-    $split =~ s/^_//smx;
-    $ids->{split} = $split;
-  }
-
-  return $ids;
+  return;
 }
 
 sub save_files {
   my $self = shift;
 
   my $count = 0;
-  my $rs = $self->schema->resultset($self->table_name);
+  my $rs = $self->schema->resultset($TABLE_NAME);
   foreach my $file (@{$self->_get_files}) {
-    my $transaction = sub {
-      my ($fname, $dir, $ext) = fileparse($file);
-      my $values = fname2ids($fname);
-
-      my $content = slurp $file;
-      $values->{file_content} = $content;
-      $values->{file_name} = $fname;
-      $values->{file_content_compressed} = compress($content);
-      _qvalues($values, $content);
-      $rs->find_or_new($values)->set_inflated_columns($values)->update_or_insert();
-		          };
-    eval {
-      $self->schema->txn_do($transaction);
-      $count++;
-      1;
-    } or do {
-      croak "Failed to load $file to the database: $EVAL_ERROR";
-    };
+    my ($fname, $dir, $ext) = fileparse($file);
+    my $values = _fname2ids($fname);
+    if ($values) {
+      my $transaction = sub {
+        my $content = slurp $file;
+        $values->{'file_content'} = $content;
+        $values->{'file_name'} = $fname;
+        $values->{'file_content_compressed'} = compress($content);
+        _qvalues($values, $content);
+        $rs->find_or_new($values)->set_inflated_columns($values)->update_or_insert();
+		  };
+      try {
+        $self->schema->txn_do($transaction);
+        $count++;
+      } catch {
+        croak "Failed to load $file to the database: $_";
+      };
+    }
   }
   return $count;
 }
@@ -145,6 +118,7 @@ sub save_files {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
 __END__
 
 =head1 NAME
@@ -157,19 +131,16 @@ npg_qc::file_store
 
 =head1 DESCRIPTION
 
-Saves into a database fastqcheck files found in the given paths. Also saves a numver of qX values.
+Saves into a database fastqcheck files found in the given paths. Also saves a number of qX values.
+Only lane-level files are saved.
 
 =head1 SUBROUTINES/METHODS
 
 =head2 path - an attribute; a reference to an array of paths where the files to be saved are located
 
-=head2 save_files 
-
 =head2 schema - an attribute; DBIx schema object for the NPG QC database
 
 =head2 save_files - finds fastqcheck files in the given paths, reads the content and loads in to NPG QC database
-
-=head2 fname2ids
 
 =head1 DIAGNOSTICS
 
@@ -187,13 +158,17 @@ Saves into a database fastqcheck files found in the given paths. Also saves a nu
 
 =item Carp
 
-=item English qw{-no_match_vars}
+=item Try::Tiny
 
 =item File::Spec
 
 =item Perl6::Slurp
 
 =item File::Basename
+
+=item MooseX::Getopt
+
+=item Compress::Zlib
 
 =item npg_qc::Schema
 
@@ -209,7 +184,7 @@ Marina Gourtovaia, E<lt>mg8@sanger.ac.ukE<gt>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2010 GRL, by Marina Gourtovaia
+Copyright (C) 2018 GRL Genome Research Ltd
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
