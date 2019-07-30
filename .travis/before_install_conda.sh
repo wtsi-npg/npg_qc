48b6131e8197549553ba14c3eb77532d7dd62140
Marina Gourtovaia <mg8@sanger.ac.uk>
2019-07-30 12:00:20 +0100

    updated miniconda repository

--- .travis/before_install_conda.sh
+++ .travis/before_install_conda.sh
@@ -1,9 +1,4 @@
-
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-    wget -q -w 30 https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-else
-    wget -q -w 30 https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-fi
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O miniconda.sh
 
 /bin/bash miniconda.sh -b -p "$HOME/miniconda"
 export PATH="$HOME/miniconda/bin:$PATH"
