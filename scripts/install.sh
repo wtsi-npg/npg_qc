a1d98187b5d470c61fbc505c490ba42929108f17
Frederick Dodd <fd6@sanger.ac.uk>
2021-03-22 11:32:13 +0000

    Change from Travis CI to Github Actions

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,19 +4,19 @@ set -e -u -x
 
 # The default build branch for all repositories. This defaults to
 # TRAVIS_BRANCH unless set in the Travis build environment.
-WTSI_NPG_BUILD_BRANCH=${WTSI_NPG_BUILD_BRANCH:=$TRAVIS_BRANCH}
+WTSI_NPG_BUILD_BRANCH=$1
+
+WTSI_NPG_GITHUB_URL=$2
 
 # CPAN as in npg_npg_deploy
+eval $(perl -I ~/perl5ext/lib/perl5/ -Mlocal::lib=~/perl5ext)
 cpanm --notest --reinstall App::cpanminus
 cpanm --quiet --notest Alien::Tidyp
 cpanm --quiet --notest LWP::Protocol::https
 cpanm --quiet --notest https://github.com/chapmanb/vcftools-cpan/archive/v0.953.tar.gz
 
-# Conda
-export PATH="$HOME/miniconda/bin:$PATH"
-conda create -q --name "$CONDA_TEST_ENV" python=$TRAVIS_PYTHON_VERSION
-conda install --name "$CONDA_TEST_ENV" npg_qc_utils
-conda install --name "$CONDA_TEST_ENV" baton
+# adding in perl5lib location for npg_qc locations
+export PERL5LIB=${WTSI_NPG_BUILD_BRANCH}/lib/npg_qc/:$PERL5LIB
 
 # WTSI NPG Perl repo dependencies
 repos=""
@@ -34,11 +34,25 @@ done
 # install
 for repo in $repos
 do
-    cd $repo
-    cpanm --quiet --notest --installdeps . || find /home/travis/.cpanm/work -cmin -1 -name '*.log' -exec tail -n20  {} \;		
-    perl Build.PL
-    ./Build		
-    ./Build install
+    export PERL5LIB=$repo/blib/lib:$PERL5LIB:$repo/lib
 done
 
-cd $TRAVIS_BUILD_DIR
+for repo in $repos
+do
+    cd $repo
+    cpanm --quiet --notest --installdeps .
+    perl Build.PL
+    ./Build
+done
+
+eval $(perl -I ~/perl5ext/lib/perl5/ -Mlocal::lib=~/perl5npg)
+
+for repo in $repos
+do
+    cd $repo
+    cpanm  --quiet --notest --installdeps .
+    ./Build install
+    perl Build.PL
+    ./Build
+done
+cd
