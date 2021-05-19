204729001f49f1c82a7e31ed168ddbb340ee6d67
jmtcsngr <jmtc@sanger.ac.uk>
2021-05-19 14:57:53 +0100

    remove specific npm version use conda default

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-set -e -x
-# set -u fails cause npm install to fail
-
-#Passing in npm version
-NPM_VERSION=$1
-
-npm install -g "npm@${NPM_VERSION}"
+set -e -x -u
 
 # Dummy executable files generated for tests use #!/usr/local/bin/bash
 sudo mkdir -p /usr/local/bin
